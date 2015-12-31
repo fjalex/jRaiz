b2b4042ab6dc89f10ebc7594d111f8f2b98a6a85
Alex Ferreira <fjalex@users.noreply.github.com>
2015-12-30 23:29:42 -0200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,39 @@
+struct = {
+	container : {
+		header : {
+			logo : {},
+			menu : {}
+		},
+		main : {
+			article1 : {},
+			article2 : {},
+			article3 : {},
+			article4 : {},
+			article5 : {}
+		},
+		sidebar : {
+			ad1 : {
+				img : {}
+			},
+			ad2 : {
+				embed : {}
+			},
+			ad3 : {
+				applet : {}
+			}
+		},
+		footer : {
+			col1 : {},
+			col2 : {},
+			col3 : {}
+		}
+	},
+	modal : {
+		button : {}
+	}
+};
+
+
+//t.init({a:64});
+//t.it(struct, t.body);
+t.it(struct);
