d7568bc091d4bd8b8303895c1f5f76a8aff701ce
Alex Ferreira <fjalex@users.noreply.github.com>
2016-01-02 18:57:14 -0200

    Update basic notation.js

--- js/basic notation.js
+++ js/basic notation.js
@@ -10,21 +10,41 @@
 	
 */
 
-
 //INIT
 t.init({
 		strict: false, // FALSE WHEN HTML 5
-		structure: OBJ, // DATA OBJ WITH STRUCTURE DIVS / SECTIONS
+		structure: { // DATA OBJ WITH STRUCTURE DIVS / SECTIONS
+			header : {
+				logo : "logo",
+				menu : {}
+			},
+			main : {
+				article : {
+					iterate : true
+				}
+			},
+			sidebar : {
+				ad1 : {},
+				ad2 : {},
+				ad3 : {},
+			}
+			footer : {
+				col1 : {},
+				col2 : {},
+				col3 : {},
+			}
+		},
 		menu: { // DATA OBJ WITH MENU LABELS AND ADDRESSES
 			home : t.page(),
 			about : t.modal(), // MODAL WINDOW
 			staff : t.page(),
 			prices : t.page(),
 			contact : t.page()
-		}
+		},
 		css: OBJ // CSS RULES TO ALL PAGES
 	});
 
+//CONFIG EACH PAGE
 t.contact.form({
 	name : ["name", "notEmpty"],
 	email : ["email", "validate", "notEmpty"],
