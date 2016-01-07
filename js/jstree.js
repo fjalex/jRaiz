80d265af1085311179223188fbab7e01b762a026
Alex Ferreira <fjalex@users.noreply.github.com>
2016-01-07 05:26:34 -0200

    Update jstree.js

--- js/jstree.js
+++ js/jstree.js
@@ -1,7 +1,7 @@
 function Tree(){
 	//PROTOTYPE REFERENCE
 	var tree = this;
-	
+
 	//DEBUG MODE
 	this.debug = false;
 	
@@ -186,6 +186,76 @@ Tree.prototype.bug = function(err){
 }
 
 /*
+		INIT FUNCTION
+*/
+Tree.prototype.init = function(config){
+	if(config == undefined)
+		return this.bug([
+			"YOU MUST CALL init() WITH A CONFIGURATION OBJECT!",
+			"SOMETHING LIKE THIS: init({var1: val, var2: val})"
+			]);
+	
+	//IF SECTIONS == TRUE, AUTOMATICALLY SETS TO HTML 5
+	if(config.sections != undefined && config.sections){
+		config.strict = false;
+		for(var section in this.sections)
+			this.tags[section] = this.sections[section];
+	}
+	
+	//IF CONFIG.STRICT == FALSE ~ HTML 5!
+	if(config.strict != undefined && !config.strict){
+		this.strict = false;
+		for(var tag in this.html5)
+			this.tags[tag] = this.html5[tag];
+		
+		var nodeDoctype = document.implementation.createDocumentType("HTML","","");
+		if(document.doctype) {
+			document.replaceChild(nodeDoctype, document.doctype);
+		} else {
+			document.insertBefore(nodeDoctype, document.childNodes[0]);
+		}
+	}
+	
+	//IF STRICT == TRUE ~ HTML 4
+	if(this.strict){
+		for(var tag in this.html4)
+			this.tags[tag] = this.html4[tag];
+	}
+	
+	if(config.external != undefined && Array.isArray(config.external) ){
+		for(var k in config.external){
+			var ext = config.external[k].slice(-4);
+			if(ext[1] == "c" && ext[2] == "s" && ext[3] == "s"){ //WHEN CSS
+				var imprt = {tag: "link", rel: "stylesheet"};
+				
+				if(ext[0] == "."){
+					imprt.href = config.external[k]; //WHEN .CSS
+				} else if(ext[0] == "|"){
+					imprt.href = config.external[k].slice(0,-4); //WHEN |CSS
+				}
+			} else if(ext[2] == "j" && ext[3] == "s"){ //WHEN JS
+				var imprt = {tag: "script", type: "text/javascript"};
+				
+				if(ext[1] == "."){
+					imprt.src = config.external[k];
+				} else if(ext[1] == "|"){
+					imprt.src = config.external[k].slice(0,-3)
+				}
+			}
+			
+			imprt = this.element(imprt);
+			document.head.appendChild(imprt);
+		}
+	}
+	
+	if(config.structure != undefined){
+		this.nodes(config.structure);
+	}
+	
+	return true;
+}
+
+/*
 		HTML ELEMENT FUNCTION
 		- VERIFIES IF THE TAG BELONGS TO STRICT
 		- VERIFIES IF THE TAG BELONGS TO HTML 5
@@ -195,14 +265,15 @@ Tree.prototype.bug = function(err){
 		- ITERATE ELEMENT ATTRIBUTES
 		- RETURN ELEMENT
 */
-Tree.prototype.element = function(){
+Tree.prototype.element = function(ops){
+	ops = ops || {};
 	ops.tag = ops.tag || "div";
 	
 	if(this.strict && this.tags[ops.tag] == undefined)
-		return tree.bug("STRICT HTML DOES NOT ALLOW THE TAG: " + ops.tag);
+		return this.bug("STRICT HTML DOES NOT ALLOW THE TAG: " + ops.tag);
 	
-	if(!this.strict && "h4" in this.tags[ops.tag])
-		return tree.bug("HTML 5 DOES NOT ALLOW THE TAG: " + ops.tag);
+	if(!this.strict && ops.tag in this.html4)
+		return this.bug("HTML 5 DOES NOT ALLOW THE TAG: " + ops.tag);
 	
 	var Element = document.createElement(ops.tag),
 			nodeName = Element.nodeName.toLowerCase();
@@ -221,33 +292,22 @@ Tree.prototype.element = function(){
 }
 
 /*
-		CREATES ONE <style> TAG
-		ADD IT TO THE DOCUMENT HEAD AS THE 1st styleSheet
-		document.styleSheets[0]
-*/
-Tree.prototype.styleSheet = function(){
-	
-}
-
-/*
-		VERIFIES IF HTML 4 OR 5
-		WHEN HTML 5, VERIFIES IF USES SECTIONS OR DIVS
-		INITIALIZES THE APP STRUCTURE
-*/
-Tree.prototype.cssOBJ = function( obj ){
-	
-}
-
-/*
 		NODES FUNCTION
+		- GETS OBJ
+		- TEST IF CONFIGURATION ($)
+		- TEST IF TEXT OR HTML CODE
+		- TEST IF TAG NAME EXISTS
+		- IF NOT A TAG NAME, CREATES A DIV
+		- APPEND NEW ELEMENT TO PARENT;
 */
 Tree.prototype.nodes = function(obj, parent){
-	parent = parent || tree.body;
-	
-	if(obj == undefined) return tree.bug("EMPTY ARGUMENT!");
+	parent = parent || this.body;
+
+	if(obj == undefined) return this.bug("EMPTY ARGUMENT!");
 	
 	if(typeof obj == "object")
 		for(var k in obj){
+			var ops = {};
 			if( k == "$"){
 				parent.className += " " + obj[k].classes;
 				parent.id = obj[k].id;
@@ -264,18 +324,117 @@ Tree.prototype.nodes = function(obj, parent){
 				continue;
 			} 
 
-			if(k in tree.tags) ops.tag = k;
+			if(k in this.tags) ops.tag = k;
 				else ops.classes = k;
 			
-			var Element = tree.element(ops);
+			var Element = this.element(ops);
 			
 			parent.appendChild(Element);
 			
-			tree.nodes(obj[k], Element);
+			this.nodes(obj[k], Element);
 		}
 	
 	return this;
 }
 
+/*
+		CREATES ONE <style> TAG
+		ADD IT TO THE DOCUMENT HEAD AS THE 1st styleSheet
+		RETURN document.styleSheets[0]
+*/
+Tree.prototype.styleSheet = function(){
+	
+}
+
+/*
+		CSSOBJ FUNCTION
+*/
+Tree.prototype.cssOBJ = function( obj ){
+	
+}
+
+/*
+		PAGE FUNCTION
+			CREATES PAGE
+			REGISTER PAGE
+			RETURN PAGE
+*/
+Tree.prototype.page = function(){
+}
+
+/*
+		MODAL FUNCTION
+			CREATES MODAL
+			REGISTER MODAL
+			RETURN MODAL
+*/
+Tree.prototype.modal = function(){
+	
+}
+
+/*
+		LOGO FUNCTION
+*/
+Tree.prototype.logo = function(ops){
+	ops = ops || {};
+	ops.id = ops.id || "logo";
+	ops.classes = ops.classes || " ";
+	ops.link = ops.link || "/";
+	ops.title = ops.title || "Home";
+	ops.h1 = ops.h1 == undefined ? true : ops.h1;
+	
+	var logo = {},
+			c = {
+				id : ops.id,
+				classes : ops.classes
+			},
+			link = {
+				href : ops.link,
+				title : ops.title
+			};
+	
+	if(ops.h1){
+		logo["h1"] = {
+			$ : c,
+			a : link
+		};
+	} else {
+		logo["a"] = link;
+		logo.a["$"] = c;
+	}
+	
+	return logo;
+}
+
+/*
+		COLOR FUNCTION
+*/
+types = {};
+types.Color = function(value){
+	this.value = value;
+	this.list = [];
+	this.change = function(newValue){
+		this.value = newValue;
+		for(var k in this.list){
+			list[k] = newValue.toString();
+		}
+	};
+	this.add = function(newItm){
+		this.list.push(newItm);
+	};
+	this.remove = function(){
+		
+	};
+	this.toString = function(){
+		return this.value;
+	};
+	
+	return this;
+}
+Tree.prototype.color = function(value){
+	return new types.Color(value);
+}
+
 t = new Tree();
-t.init();
+t.debug = true;
+//t.init();
