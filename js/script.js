8cac016a5abc6897b3d9b8fc02cd1c6ca9a3544b
Alex Ferreira <fjalex@users.noreply.github.com>
2016-01-24 02:14:29 -0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ cssRules  = {
 		$bWidth : 5,
 		$mainColor : "#FCADEB"
 	},
+	body : {
+		//backgroundColor : "lightgrey",
+		//padding: '50px'
+	},
 	".container>div" : {
 		backgroundColor: "orange",
 		float : "left",
@@ -50,21 +54,47 @@ cssRules  = {
 		border : "solid {$bWidth * 2 + px } $mainColor",
 		backgroundColor : "$mainColor",
 		color : "$mainColor",
-		
 	}
 };
 
+
 struct = {
+	modal : {
+		button : {
+			$ : {
+				onclick : function(e){console.log(e)},
+				classes : "super classes",
+				id : "inpID",
+				text : "SUPER BUTTON",
+				type : "submit"
+			}
+		}
+	},
+	menubar : {
+		$ : {
+			classes : "fullw"
+		},
+		header : {
+			$ : {}
+		},
+		menu : j.menu({
+			home : {url : "#",},
+			a_empresa : {url : "#", label : "A Empresa"},
+			produtos : {url : "#",},
+			localizacao : {url : "#", label : "Localização"},
+			contato : {url : "#",},
+		}),
+	},
 	container : {
 		$ : {
-			classes : "criatr lska",
+			classes : "class01 class02",
 			id : "superDiv",
 			css : {
 				//marginA : "30px 40px",
 				//margin : "30px {30 +20}",
 				//backgroundColor : "$lightColor",
 				//border : "solid {$bwidth + $ouVar} $lightColor $otherVar $tVar" //REGISTER VAR, WHEN UNDEFINED
-				border : "$var1 $var2 $var3"
+				//border : "$var1 $var2 $var3"
 			},
 			//$var1 : 465, //REGISTER VAR, WHEN UNDEFINED AND ASSIGN VALUE
 			//$var2 : "200px",
@@ -76,7 +106,8 @@ struct = {
 		},
 		header : {
 			$ : {
-				dir: "ltr",//'ltr', //TRY/CATCH ERROR
+				classes : "cols col_12",
+				dir: "ltr",
 				onclick : function(e){
 					console.log(e,e.offsetX, e.x, e.clientX, e.layerX, e.movementX);
 				},
@@ -85,19 +116,20 @@ struct = {
 					//fontFamily : "$fontFamily",
 					//background : "url('/img/headerbg.jpg') no-repeat center top $lightColor",
 					//color : "$lightColor"
-					border : "solid $var2 $var3",
+					//border : "solid $var2 $var3",
 					//backgroundColor : "$var3"
 				}
 			},
-			logo : {},
+			logo : j.logo(),
 			menu : {}
 		},
 		main : {
 			$ : {
+				classes : "cols col_8",
 				css : {
 					//backgroundColor : "$lightColor"
 					//backgroundColor : "$var3",
-					border : "solid $var2 $var3",
+					//border : "solid $var2 $var3",
 				}
 			},
 			article1 : {
@@ -146,11 +178,34 @@ struct = {
 					}
 				})
 			},
-			article3 : {},
-			article4 : {},
+			article3 : {
+				h1 : { $ : {text : "header 1"}},
+				h2 : { $ : {text : "header 2"}},
+				h3 : { $ : {text : "header 3"}},
+				h4 : { $ : {text : "header 4"}},
+				h5 : { $ : {text : "header 5"}},
+				h6 : { $ : {text : "header 6"}},
+			},
+			article4 : {
+				t1 : {
+					$ : {
+						tag : "p",
+						text : "dictum at. Nam euismod pulvinar ante sed iaculis. Vestibulum euismod, lacus nec condimentum dapibus, lectus nibh imperdiet neque, non dapibus dui augue id ante. Phasellus tristique turpis eu lectus luctus, quis eleifend lacus pretium. Phasellus et arcu malesuada, consectetur leo ut, molestie ante. Nunc bibendum ultrices metus, a mattis enim pretium accumsan. Quisque eget aliquet felis. Quisque viverra magna mauris, a accumsan dolor gravida at. Nunc non rhoncus libero. Cras consectetur mauris nec risus congue eleifend. Ut egestas tristique accumsan. Praesent finibus felis sem, ut suscipit neque accumsan ut."
+					}
+				},
+				t2 : {
+					$ : {
+						tag : "p",
+						text : "dictum at. Nam euismod pulvinar ante sed iaculis. Vestibulum euismod, lacus nec condimentum dapibus, lectus nibh imperdiet neque, non dapibus dui augue id ante. Phasellus tristique turpis eu lectus luctus, quis eleifend lacus pretium. Phasellus et arcu malesuada, consectetur leo ut, molestie ante. Nunc bibendum ultrices metus, a mattis enim pretium accumsan. Quisque eget aliquet felis. Quisque viverra magna mauris, a accumsan dolor gravida at. Nunc non rhoncus libero. Cras consectetur mauris nec risus congue eleifend. Ut egestas tristique accumsan. Praesent finibus felis sem, ut suscipit neque accumsan ut."
+					}
+				}
+			},
 			article5 : {}
 		},
 		sidebar : {
+			$ : {
+				classes : "cols col_4",
+			},
 			ad1 : {
 				img : {}
 			},
@@ -163,27 +218,31 @@ struct = {
 		},
 		"div#elmID.class1.class2" : {}, // TEST
 		footer : {
-			col1 : {},
-			col2 : {},
-			col3 : {}
-		}
-	},
-	modal : {
-		button : {
 			$ : {
-				onclick : function(e){console.log(e)},
-				classes : "super classes",
-				id : "inpID",
-				text : "SUPER BUTTON",
-				type : "submit"
+				//classes : "cols col_12",
+				classes : "clear",
+			},
+			col1 : {
+				$ : {classes : "cols col_4"}
+			},
+			col2 : {
+				$ : {classes : "cols col_4"}
+			},
+			col3 : {
+				$ : {classes : "cols col_4"}
 			}
 		}
-	}
+	},
 };
+//*/
 
 //j.init({strict:false, sections:true});
+
 j.init({strict:true, sections:false});
 j.nodes(struct);
+//j.nodes(j.bGrid);
 
-var a = new j.Sheet(cssRules);
-//console.info(documenj.styleSheets);
+//console.log(j.basicCSS);
+//var b = new j.Sheet(cssRules);
+console.info(document.styleSheets);
+//console.info(a);
